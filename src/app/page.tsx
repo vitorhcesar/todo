e62eaa2f99ccaf6b6659859959177a8ea1da9d4a
Vitor <thevitor.hugo27@gmail.com>
2023-05-22 10:28:25 -0300

    Commit 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,67 @@
-import Image from 'next/image'
+'use client'
+
+import Todo from '@/components/todo';
+import { useState } from 'react';
+import { IoMdAddCircle } from 'react-icons/io'
+
+class todo {
+    feito: boolean;
+    desc: string;
+
+    constructor(feito: boolean, desc: string){
+        this.feito = feito;
+        this.desc = desc;
+    }
+}
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    const [ todos, setTodos ] = useState([new todo(false, 'Exemplo de todo não feito'), new todo(true, 'Exemplo de todo feito')]);
+    const [ input, setInput ] = useState('Nova tarefa');
+
+    const addNewTodo = () => {
+        if(input == ''){
+            return;
+        }
+
+        let newTodo = new todo(false, input);
+
+        setTodos([...todos, newTodo]);
+        setInput('');
+        console.log(todos);
+	}
+
+    function showMessage(){
+        if(todos.length == 0){
+            console.log('Ola');
+            return (<p>Não existem todos, crie o seu <b>primeiro</b>!</p>)
+        }
+    }
+
+    return (
+        <div className="flex min-h-screen flex-col items-center gap-20 p-10">
+            <header>
+                <h1 className='text-8xl'>🐨 Lista de tarefas 🐼</h1>
+            </header>
+            <main className='flex flex-col items-center justify-center w-screen'>
+                <div className='flex items-center gap-2'>
+                    <input 
+                        className='border-2 border-purple-600 outline-purple-800 p-2 rounded-lg
+                        text-2xl'
+                        onChange={(e) => {
+                            setInput(e.target.value);
+                        }}
+                        value={input} />
+                    <button onClick={() => addNewTodo()}><IoMdAddCircle className='text-5xl text-purple-700 hover:text-purple-900 transition-all duration-500' /></button>
+                </div>
+                <div className='w-screen pt-5'>
+                    <ul id='map' className='flex flex-col items-center justify-center gap-3'>
+                        {todos.map((e, index: number) => {
+                            return (<Todo key={index} index={index} desc={e.desc} feito={e.feito} todos={todos} setTodos={setTodos} />);
+                        })}
+                        {showMessage()}
+                    </ul>
+                </div>
+            </main>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    );
 }
---
+++ src/components/todo.tsx
@@ -0,0 +1,70 @@
+'use client'
+import React, { Dispatch, SetStateAction } from 'react'
+import { BsTrash3Fill } from 'react-icons/bs'
+
+interface todo {
+    index: number,
+    desc: string,
+    feito: boolean,
+    todos: Array<todoItem>,
+    setTodos: Dispatch<SetStateAction<Array<todoItem>>>
+}
+
+interface todoItem {
+    desc: string,
+    feito: boolean
+}
+
+export default function Todo( {index, desc, feito, todos, setTodos}:  todo) {
+    function changeCheck(){
+        let newTodos:Array<todoItem> = todos.filter((e) => {
+            if(todos[index] == e){
+                e.feito = !e.feito;
+                feito = !feito;
+
+                return e;
+            } else {
+                return e;
+            }
+        });
+
+        console.log(newTodos);
+
+        setTodos(newTodos);
+    }
+
+    function eraseTodo(){
+        let newTodos:Array<todoItem> = todos.filter((e) => {
+            if(todos[index] == e){
+            } else {
+                return e;
+            }
+        });
+
+        console.log(newTodos);
+        setTodos(newTodos);
+    }
+
+    if(feito){
+        return (
+            <li className='flex gap-3 w-1/2 items-center justify-between p-4 bg-purple-200 rounded-lg transition-all'>
+                <div className='flex items-center gap-2'>
+                    <input type="checkbox" name={desc} checked={feito} onChange={changeCheck} />
+                    <p className='text-3xl text-slate-600 line-through'>{desc}</p>
+                </div>
+                <button onClick={() => eraseTodo()}><BsTrash3Fill className='text-3xl self-end text-slate-600 hover:text-slate-800 transition-all' /></button>
+            </li>
+        );
+    } else {
+        return (
+            <li className='flex gap-3 w-1/2 items-center justify-between p-4 bg-purple-400 rounded-lg transition-all'>
+                <div className='flex items-center gap-2'>
+                    <input type="checkbox" name={desc} checked={feito} onChange={changeCheck} />
+                    <p className='text-3xl text-slate-50'>{desc}</p>
+                </div>
+                <button onClick={() => eraseTodo()}><BsTrash3Fill className='text-3xl self-end text-slate-100 hover:text-slate-400 transition-all' /></button>
+            </li>
+        );
+    }
+    
+}
